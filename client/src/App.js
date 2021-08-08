753683bea161df5c6729c5572e11bf32d5c0029d
Brett Treweek <bretttrew@gmail.com>
2021-08-08 12:45:52 +0800

    started setting up signup/login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Map from './components/map';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
       <Map/>
+      <Signup/>
     </div>
   );
 }
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,7 @@
 const { Round, User, Hazard } = require("../models");
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
 
 const resolvers = {
   Query: {
@@ -20,6 +23,40 @@ const resolvers = {
       return await User.find;
     },
   },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return User.findByIdAndUpdate(context.user.id, args, {
+          new: true,
+        });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  }
 };
 
 module.exports = resolvers;
